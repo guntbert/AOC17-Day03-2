c2faacfb9c8c143785cce4924deb3a431d39c0f5
G. Reiter <guntbert@gmx.at>
2018-03-28 23:27:32 +0200

    Start creating helper methods
    for determining the state of any "tile" in the grid/pattern

--- Program.cs
+++ Program.cs
@@ -12,5 +12,45 @@ namespace Day03_2a
 
 
         }
+
+        // 
+    }
+
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+    class Environment
+    {
+        bool isPlaceOnTheLeftEmpy(Dictionary<Point, int> currentPattern, Point currentLocation)
+        {
+            Point checkThis = new Point { X = currentLocation.X - 1, Y = currentLocation.Y };
+            return currentPattern.ContainsKey(checkThis);
+
+        }
+
+        long DetermineValueOfTile(Dictionary<Point, int> currentPattern, Point currentLocation)
+        {
+            long sum = 0;
+            // einmal rundherum
+            int x0 = currentLocation.X, y0 = currentLocation.Y;
+            // check: (x0-1,y0-1),(x0-1,y0),(x0-1,y0+1),(x0,y0-1),...
+            // left side
+            for (int xCheck = x0 - 1; xCheck <= x0 + 1; xCheck += 2)
+            {
+                for (int yCheck = -1; yCheck <= 1; yCheck++)
+                {
+                    Point checkThis = new Point(xCheck, yCheck);
+                    if (currentPattern.ContainsKey(checkThis))
+                        sum += currentPattern[currentLocation];
+                }
+            }
+            return sum;
+
+        }
     }
 }
+
